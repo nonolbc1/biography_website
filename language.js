c01f4f1e797ffd26305120feb3dfd542b3e9eb62
Nonolbc1 <105375209+nonolbc1@users.noreply.github.com>
2025-11-22 18:52:41 +0100

    New animation and translation

--- language.js
+++ language.js
@@ -4,14 +4,16 @@ const translations = {
         language:"Langue:",
         title_bio:"Biographie de Nonolbc1",
         h1_desc:"Description",
-        p1_desc:"Je fais des vidéos sur Youtube et Tiktok sur le thème de Minecraft. Mes passions sont de développer certains petits jeux ou logiciels. Je joue beaucoup aux jeux vidéos, je modélise certaines choses et j'aime bien faire du sport (de l'escalade, du basket, etc...)."
+        p1_desc:"Je fais des vidéos sur Youtube et Tiktok sur le thème de Minecraft. Mes passions sont de développer certains petits jeux ou logiciels. Je joue beaucoup aux jeux vidéos, je modélise certaines choses et j'aime bien faire du sport (de l'escalade, du basket, etc...).",
+        sourceCode:"Code source"
     },
     en: {
         title:"Website by ©Nonolbc1",
         language:"Language:",
         title_bio:"Biography of Nonolbc1",
         h1_desc:"Description",
-        p1_desc:"I make videos on Youtube and Tiktok on the theme of Minecraft. My passions are developing certain small games or software. I play a lot of video games, I model certain things and I like to do sports (climbing, basketball, etc...)."
+        p1_desc:"I make videos on Youtube and Tiktok on the theme of Minecraft. My passions are developing certain small games or software. I play a lot of video games, I model certain things and I like to do sports (climbing, basketball, etc...).",
+        sourceCode:"Source code"
     }
 }
 
@@ -21,6 +23,7 @@ let t2 = document.getElementById("_00001");
 let t3 = document.getElementById("_00002");
 let t4 = document.getElementById("_00003");
 let t5 = document.getElementById("_00004");
+let t6 = document.getElementById("_00005");
 
 languageSelectop.addEventListener("change", (event) => {
     setLanguage(event.target.value)
@@ -33,11 +36,13 @@ const setLanguage = (language) => {
         t3.innerText = translations.fr.title_bio
         t4.innerText = translations.fr.h1_desc
         t5.innerText = translations.fr.p1_desc
+        t6.innerText = translations.fr.sourceCode
     } else if (language == "en") {
         t1.innerText = translations.en.title
         t2.innerText = translations.en.language
         t3.innerText = translations.en.title_bio
         t4.innerText = translations.en.h1_desc
         t5.innerText = translations.en.p1_desc
+        t6.innerText = translations.en.sourceCode
     }
 }
